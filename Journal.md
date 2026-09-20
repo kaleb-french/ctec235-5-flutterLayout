18f2a35b2d5b121502abe47b3d2e060e94ce57a4
Kaleb French <your-email@example.com>
2026-09-19 20:42:17 -0500

    phase 4- complete
    phase 4-
    The grid view with the crossAsixCount setting seems to be alot easier in
    my opinion. To get this 3x3 grid the GridView.count() is the way to go,
    while not as flexible as the expanded nesting it is way easier to setup.
    for a true grid view this creates a even gride with fewer lines, but if
    you want a non-standard grid, this is where the nested expanded widgets
    will do better.

--- Journal.md
+++ Journal.md
@@ -13,3 +13,12 @@ Phase 1-
     - assets/images/mike.jpg
     - assets/images/peter.jpg
 Is exactly how it is in the yaml 2x space to indent within flutter: the 2x more space for each asset declaration. If not container constrait is placed on an image it can take up more space than intented. especially with larger images then can coninue on off the screen. The BoxFit.cover scales the image to fit in a container while trying to maintain aspect ratio. 
+
+Phase 2-
+If you take away expanded from one of the tiles it takes up more space due to it not being evenly divided with the others anymore. The expanded works with the row to figure out an even spacing for the elements using expanded and uses this height and width when displayed the wraped widgets.
+
+Phase 3-
+Nesting the expanded widgets for the rows and rows child widgets follows the golden rule, because it puts even contraints on the height and width of each bradyTile to fit evenly on screen.
+
+phase 4-
+The grid view with the crossAsixCount setting seems to be alot easier in my opinion. To get this 3x3 grid the GridView.count() is the way to go, while not as flexible as the expanded nesting it is way easier to setup. for a true grid view this creates a even gride with fewer lines, but if you want a non-standard grid, this is where the nested expanded widgets will do better.
--- lib/main.dart
+++ lib/main.dart
@@ -76,26 +76,19 @@ class _MyHomePageState extends State<MyHomePage> {
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
     return Scaffold(
-      body: Column(
-                children: [   Expanded(child: Row(
-                                    children: [Expanded(child: BradyTile(imagePath: "assets/images/alice.jpg")),
-                                               Expanded(child:BradyTile(imagePath: "assets/images/greg.jpg")),
-                                               Expanded(child: BradyTile(imagePath: "assets/images/jan.jpg")),]
-                                )
-                              ),
-                              Expanded(child: Row(
-                                    children: [Expanded(child: BradyTile(imagePath: "assets/images/cindy.jpg")),
-                                               Expanded(child:BradyTile(imagePath: "assets/images/carol.jpg")),
-                                               Expanded(child: BradyTile(imagePath: "assets/images/marcia.jpg")),]
-                                )
-                              ),
-                              Expanded(child: Row(
-                                    children: [Expanded(child: BradyTile(imagePath: "assets/images/bobby.jpg")),
-                                               Expanded(child:BradyTile(imagePath: "assets/images/mike.jpg")),
-                                               Expanded(child: BradyTile(imagePath: "assets/images/peter.jpg")),]
-                                )
-                              ),
-                            ]
+      body: GridView.count(crossAxisCount: 3,
+                            crossAxisSpacing: 0,
+                            mainAxisSpacing: 0,
+                            children: [Expanded(child: BradyTile(imagePath: "assets/images/alice.jpg")),
+                                               Expanded(child: BradyTile(imagePath: "assets/images/greg.jpg")),
+                                               Expanded(child: BradyTile(imagePath: "assets/images/jan.jpg")),
+                                               Expanded(child: BradyTile(imagePath: "assets/images/cindy.jpg")),
+                                               Expanded(child: BradyTile(imagePath: "assets/images/carol.jpg")),
+                                               Expanded(child: BradyTile(imagePath: "assets/images/marcia.jpg")),
+                                               Expanded(child: BradyTile(imagePath: "assets/images/bobby.jpg")),
+                                               Expanded(child: BradyTile(imagePath: "assets/images/mike.jpg")),
+                                               Expanded(child: BradyTile(imagePath: "assets/images/peter.jpg")),],
+
       )
     );
   }
